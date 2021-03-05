ac3f9eeedea2b798b09545471aa2f4d5ec76b54b
Lev-etd <levevt2012@gmail.com>
2021-03-05 21:10:48 +0300

    OMG, I hate C++

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
-
-
 #include <iostream>
 #include <cstdlib>
-#include <limits>
 #include <cmath>
 
 
@@ -12,17 +9,16 @@ const int kBits = 8;
 
 class BinaryOps {
     friend std::ostream &operator<<(std::ostream &out, const BinaryOps &a);
+
     friend BinaryOps operator+(const BinaryOps &a, const BinaryOps &b);
 
-    friend BinaryOps operator*(const BinaryOps& a, const BinaryOps& b);
+    friend BinaryOps operator*(const BinaryOps &a, const BinaryOps &b);
 
     friend BinaryOps operator-(const BinaryOps &a, BinaryOps &b);
 
 private:
-    int m_dec_num{};
     int m_binary_num[kBits]{};
 public:
-//    int& operator[] (int index);
     const int &operator[](int index) const;
 
     friend std::ostream &operator<<(std::ostream &out, const BinaryOps &a) {
@@ -37,17 +33,7 @@ public:
     }
 
 
-
     BinaryOps() = default;
-
-    void InputVariable() {
-        std::cout << "Enter the number: ";
-        while (!(std::cin >> m_dec_num)) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Invalid input.  Try again: ";
-        }
-    }
 
 
     void FillArray(int n) {
@@ -77,7 +63,6 @@ public:
                 }
             }
 
-            // Looping from right to left
             for (int position = 0; position <= kBits - 1; position++) {
                 if (m_binary_num[position] == 0) {
                     m_binary_num[position] = 1;
@@ -88,25 +73,7 @@ public:
         }
     }
 
-
-
-
-    int getDec() const { return m_dec_num; }
-
-    const int *getBinaryNum() const {
-        return m_binary_num;
-    }
-
-
 };
-
-const int &BinaryOps::operator[](int index) const {
-    return m_binary_num[index];
-}
-
-
-
-
 
 
 BinaryOps signed2Complement2(const BinaryOps &arr) {
@@ -139,13 +106,13 @@ BinaryOps signed2Complement2(const BinaryOps &arr) {
 int BinToDec(const BinaryOps &arr) {
     int btdbits = kBits;
     int decimal{};
-    if (arr[kBits-1] == 0) {
+    if (arr[kBits - 1] == 0) {
         for (int j = kBits - 1; j != -1; j--) {
             btdbits--;
             decimal += arr[j] * ((int) pow(2, btdbits));
         }
         return decimal;
-    } else{
+    } else {
         for (int j = kBits - 1; j != -1; j--) {
             btdbits--;
             decimal += arr[j] * ((int) pow(2, btdbits));
@@ -155,19 +122,21 @@ int BinToDec(const BinaryOps &arr) {
 }
 
 
-
+const int &BinaryOps::operator[](int index) const {
+    return m_binary_num[index];
+}
 
 BinaryOps operator+(const BinaryOps &a, const BinaryOps &b) {
     BinaryOps result;
     int carry[kBits];
     int c = 0;
     for (int u = 0; u < kBits; u++) {
-        result.m_binary_num[u] = ((a[u] ^ b[u]) ^ c); // c is carry
+        result.m_binary_num[u] = ((a[u] ^ b[u]) ^ c);
         c = ((a[u] & b[u]) | (a[u] & c)) | (b[u] & c);
         carry[u] = c;
     }
-    if(carry[kBits-1]^carry[kBits-2]){
-        throw std::runtime_error("OVERFLOW!!!!");
+    if (carry[kBits - 1] ^ carry[kBits - 2]) {
+        throw "OVERFLOW!!!!";
     }
     return result;
 }
@@ -183,25 +152,24 @@ BinaryOps operator-(const BinaryOps &a, BinaryOps &b) {
         c = ((a[u] & complement[u]) | (a[u] & c)) | (complement[u] & c);
         borrow[u] = c;
     }
-    if(borrow[kBits-1]^borrow[kBits-2]){
-        throw std::runtime_error("OVERFLOW!!!!");
+    if (borrow[kBits - 1] ^ borrow[kBits - 2]) {
+        throw "OVERFLOW!!!!";
     }
     return result_min;
 }
 
 
-
 BinaryOps operator*(const BinaryOps &a, const BinaryOps &b) {
     BinaryOps result_mult;
     int multiplicator;
-    int sign = (a[kBits-1] ^ b[kBits-1]);
+    int sign = (a[kBits - 1] ^ b[kBits - 1]);
     multiplicator = std::abs(BinToDec(a));
     while (multiplicator > 0) {
         result_mult = result_mult + b;
         multiplicator--;
     }
     if (!sign) {
-        result_mult[kBits-1] = sign;
+        result_mult[kBits - 1] = sign;
         return result_mult;
     } else {
         return result_mult;
@@ -210,63 +178,17 @@ BinaryOps operator*(const BinaryOps &a, const BinaryOps &b) {
 
 
 
-
-
-
-//int main() {
-//
-//    BinaryOps dec_a{};
-//    BinaryOps dec_b{};
-//
-//    dec_a.InputVariable();
-//    dec_b.InputVariable();
-//
-//    dec_a.DecToBinary(dec_a.getDec());
-//    dec_b.DecToBinary(dec_b.getDec());
-//
-//    std::cout << dec_a;
-//    std::cout << dec_b;
-//
-//    std::cout << (dec_a + dec_b);
-//    std::cout << (dec_a - dec_b);
-//    std::cout << (dec_a * dec_b);
-//
-//
-//    return 0;
-//}
-// TODO Классы, переполнение у умножения, названия классов и функций, хардкод в проверке, проверка не через десятичные числа, а через переполнение в бинарных
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Stack type
-class Stack
-{
+// Stack
+class Stack {
 public:
     int top;
     unsigned capacity;
-    int* array;
+    int *array;
 };
 
 // Stack Operations
-Stack* createStack( unsigned capacity )
-{
-    auto* stack = new Stack();
-
-    if (!stack) return NULL;
+Stack *createStack(unsigned capacity) {
+    auto *stack = new Stack();
 
     stack->top = -1;
     stack->capacity = capacity;
@@ -277,69 +199,54 @@ Stack* createStack( unsigned capacity )
     return stack;
 }
 
-int isEmpty(Stack* stack)
-{
-    return stack->top == -1 ;
+int isEmpty(Stack *stack) {
+    return stack->top == -1;
 }
 
-int peek(Stack* stack)
-{
+int peek(Stack *stack) {
     return stack->array[stack->top];
 }
 
-int pop(Stack* stack)
-{
+int pop(Stack *stack) {
     if (!isEmpty(stack))
-        return stack->array[stack->top--] ;
-    return '$';
+        return stack->array[stack->top--];
+    return 'b';
 }
 
-void push(Stack* stack,int op)
-{
+void push(Stack *stack, int op) {
     stack->array[++stack->top] = op;
 }
 
 
-// The main function that returns value
-// of a given postfix expression
-int evaluatePostfix(char* exp)
-{
-    // Create a stack of capacity equal to expression size
-    Stack* stack = createStack(strlen(exp));
+//evaluate
+int evalPost(char *exp) {
+
+    Stack *stack = createStack(strlen(exp));
     int i;
 
-    // See if stack was created successfully
     if (!stack) return -1;
 
-    // Scan all characters one by one
-    for (i = 0; exp[i]; ++i)
-    {
-        //if the character is blank space then continue
-        if(exp[i] == ' ')continue;
+    for (i = 0; exp[i]; ++i) {
 
-            // If the scanned character is an
-            // operand (number here),extract the full number
-            // Push it to the stack.
-        else if (isdigit(exp[i]))
-        {
-            int num=0;
+        if ((exp[i] >= 'a' && exp[i] <= 'z') ||
+            (exp[i] >= 'A' && exp[i] <= 'Z')) {
+            throw "No letters allowed!";
+        }
 
-            //extract full number
-            while(isdigit(exp[i]))
-            {
-                num = num * 10 + (int)(exp[i] - '0');
+        if (exp[i] == ' ')continue;
+
+        else if (isdigit(exp[i])) {
+            int num = 0;
+
+
+            while (isdigit(exp[i])) {
+                num = num * 10 + (int) (exp[i] - '0');
                 i++;
             }
             i--;
 
-            //push the element in the stack
-            push(stack,num);
-        }
-
-            // If the scanned character is an operator, pop two
-            // elements from stack apply the operator
-        else
-        {
+            push(stack, num);
+        } else {
             int val1 = pop(stack);
             int val2 = pop(stack);
             BinaryOps val1_bin;
@@ -349,25 +256,25 @@ int evaluatePostfix(char* exp)
             val1_bin.DecToBinary(val1);
             val2_bin.DecToBinary(val2);
 
-            switch (exp[i])
-            {
-                case '+' :{
+            switch (exp[i]) {
+                case '+' : {
                     op_result = val1_bin + val2_bin;
                     int dec_result = BinToDec(op_result);
+
                     push(stack, dec_result);
                     break;
                 }
-                case '-':
-                {
+                case '-': {
                     op_result = val1_bin - val2_bin;
                     int dec_result = BinToDec(op_result);
+
                     push(stack, dec_result);
                     break;
                 }
-                case '*':
-                {
+                case '*': {
                     op_result = val1_bin * val2_bin;
                     int dec_result = BinToDec(op_result);
+
                     push(stack, dec_result);
                     break;
                 }
@@ -375,13 +282,24 @@ int evaluatePostfix(char* exp)
             }
         }
     }
+
     return pop(stack);
 }
 
 
-int main()
-{
-    char exp[] = "10 30 +";
-    std::cout << evaluatePostfix(exp);
+
+int main() {
+    BinaryOps result;
+    try {
+        std::string input;
+        std::getline(std::cin, input);
+        char exp[input.length()];
+        strcpy(exp, input.c_str());
+        result.DecToBinary(evalPost(exp));
+        std::cout << "result decimal number is " << evalPost(exp)<< std::endl;
+        std::cout <<"result binary number is " << result << std::endl;
+    } catch (const char *expression) {
+        std::cerr << "Error: " << expression << std::endl;
+    }
     return 0;
 }
